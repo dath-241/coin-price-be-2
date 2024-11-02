7cfbbb97ddb9a48e41e098772ec71d0c34821b47
bachshadow <131352938+bachshadow@users.noreply.github.com>
2024-11-02 21:21:16 +0700

    Implement Continue Button

--- dev_frontend/src/app/(authentication)/components/ContinueButton.tsx
+++ dev_frontend/src/app/(authentication)/components/ContinueButton.tsx
@@ -6,5 +6,7 @@ interface Props {
 
 export default function ContinueButton({ onClick }: Props) {
   // TODO: Implement the continue button component
-  return <button>Continue</button>;
+  const buttonClasses = "w-1/3 text-2xl font-bold bg-[#0094FF] text-white p-6 rounded-xl flex gap-4 hover:bg-blue-300 hover:text-black text-center flex justify-center items-center";
+
+  return <button onClick={onClick} className={buttonClasses}>Continue</button>;
 }
